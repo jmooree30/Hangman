e83d7c809540909f76bef04b500c7af4fec974b0
Jacob <jmooree30@gmail.com>
2017-08-20 22:25:01 -0400

    Game done, need to serialize

--- hangman.rb
+++ hangman.rb
@@ -1,13 +1,13 @@
 load 'display.rb'
 
 class Hangman
- attr_accessor :name
- @name = name
+  attr_accessor :name
+  @name = name
 
-def initialize
-puts "What is your name?"
-@name = gets.chomp
-puts "
+  def initialize
+    puts "What is your name?"
+    @name = gets.chomp
+    puts "
 ################################################
                    HANGMAN
 ################################################
@@ -28,39 +28,57 @@ puzzle before time runs out!
 
 
 "
-Gameplay.new
-end
+  Gameplay.new
+  end
 end
 
 class Gameplay
-def initialize
-$array = []
-File.foreach('5text.txt') do |x| $array << x if (x.chomp.length >= 5 and x.chomp.length <= 12) end
-$random_word = $array.sample
-$random_word2 = $random_word.split(//)
-puts $random_word2.join
+  def initialize
+    @array = []
+    File.foreach('5text.txt') do |x|
+      chomped = x.chomp
+      @array << chomped if (chomped.length >= 5 and chomped.length <= 12)
+    end
+    @random_word = @array.sample
+    @random_word2 = @random_word.split(//)
+    @cipher = @random_word.gsub(/[a-z]/, '*').split(//)
+    puts @cipher.join
 
-$cipher = $random_word.gsub(/[a-z]/, '*').split(//)
-puts $cipher.join
-
-def choice(n)
-  $random_word2.each_with_index do |i,index|
-    if i == n
-    $cipher[index] = i 
+  def choice(n)
+    @random_word2.each_with_index do |i,index|
+      if i == n
+        @cipher[index] = i 
+      end 
     end 
-  end 
-    if $random_word2.include?(n) == false
-    $counter -= 1
-    display
-    puts "incorrect guess"
-    end 
-  puts $cipher.join
-end
+      if n == @random_word2.join.to_s
+        puts "You win"
+        puts "would you like to start another game? Y/N"
+        new_game = gets.chomp
+        if new_game == "Y"
+          Hangman.new
+          else exit 
+        end
+      end
+      if @random_word2.include?(n) == false
+        $counter -= 1
+        display
+        puts "#{$counter} guesses remaining."
+      end
+      if $counter == 0
+        puts "would you like to start another game? Y/N"
+        new_game = gets.chomp
+        if new_game == "Y"
+          Hangman.new
+          else exit 
+        end
+      end 
+        puts @cipher.join 
+  end
 
 
 $counter = 5
 while $counter > 0
-choice(gets.chomp)
+  choice(gets.chomp)
 end
 end
 end
