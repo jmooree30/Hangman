fd4ad16a2ad84f558aaea1d72362e49d8e6d96fd
Jacob <jmooree30@gmail.com>
2017-08-23 00:01:23 -0400

    Operation serialize in progress

--- hangman.rb
+++ hangman.rb
@@ -29,25 +29,41 @@ puzzle before time runs out!
 
 
 "
-  Gameplay.new
+  
   end
 end
 
 class Gameplay
-  attr_accessor :cipher, :random_word2, :counter
-  @cipher = nil
-  @random_word2 = nil
-  @counter = nil
+  attr_accessor :array, :filestuff, :random_word, :cipher, :random_word2, :counter
   def initialize
-    array = []
-    File.foreach('5text.txt') do |x|
+  @array = []
+  @filestuff = File.foreach('5text.txt') do |x|
       chomped = x.chomp
-      array << chomped if (chomped.length >= 5 and chomped.length <= 12)
+      @array << chomped if (chomped.length >= 5 and chomped.length <= 12)
     end
-    random_word = array.sample
-    @random_word2 = random_word.split(//)
-    @cipher = random_word.gsub(/[a-z]/, '*').split(//)
-    puts @cipher.join
+  @random_word = @array.sample
+  @cipher = @random_word.gsub(/[a-z]/, '*').split(//)
+  @random_word2 = @random_word.split(//)
+  @counter = 5
+  
+  def to_json
+    JSON.generate({array: @array, filestuff: @filestuff, random_word: @random_word, cipher: @cipher, random_word2: @random_word2, counter: @counter})
+  end
+
+  def load
+    game_file = File.read("saved.json")
+    data = JSON.parse(game_file)
+    @cipher = data["cipher"]
+    @random_word2 = data["random_word2"]
+    @counter = data["counter"]
+  end
+  
+
+  def save_game(string)
+    game_file = File.open("saved.json","w") do 
+    game_file.write(string)
+    end 
+  end
 
   def choice(n)
     @random_word2.each_with_index do |i,index|
@@ -68,12 +84,18 @@ class Gameplay
         @counter -= 1
         display
         puts "#{@counter} guesses remaining."
+        puts "To save press 1"
+        save = gets.chomp
+        if save == "1"
+          
+#Will not save 
+          save_game($b.to_json)
+        end
       end
       if @counter == 0
         puts "would you like to start another game? Y/N"
         new_game = gets.chomp
         if new_game == "Y"
-          Hangman.new
           else exit 
         end
       end 
@@ -88,6 +110,7 @@ class Gameplay
  end 
 end
 Hangman.new
+$b = Gameplay.new
 
 
 
