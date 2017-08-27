c00c93ba9ae0be33a95dc93b8030f0532d2f2fbe
Henré Botha <henrebotha@gmail.com>
2017-08-27 10:01:16 +0200

    Syntax cleanup

--- display.rb
+++ display.rb
@@ -1,53 +1,64 @@
 def display
-		
-		if @counter == 5
-		puts "  ______"
-		puts "  |   \\|"
-		puts "       |"
-		puts "       |"
-		puts "       |"
-		puts "       |"
-		puts "-------^-------"
-		elsif @counter == 4
-		puts "  ______"
-		puts "  |   \\|"
-		puts "  O    |"
-		puts "       |"
-		puts "       |"
-		puts "       |"
-		puts "-------^-------"
-		elsif @counter == 3
-		puts "  ______"
-		puts "  |   \\|"
-		puts "  O    |"
-		puts "  |    |"
-		puts "       |"
-		puts "       |"
-		puts "-------^-------"
-		elsif @counter == 2 
-		puts "  ______"
-		puts "  |   \\|"
-		puts "  O    |"
-		puts " -|    |"
-		puts "       |"
-		puts "       |"
-		puts "-------^-------"
-		elsif @counter == 1
-		puts "  ______"
-		puts "  |   \\|"
-		puts "  O    |"
-		puts " -|-   |"
-		puts "       |"
-		puts "       |"
-		puts "-------^-------"
-		else
-		puts "  ______"
-		puts "  |   \\|"
-		puts "  O    |"
-		puts " -|-   |"
-		puts "  ^    |"
-		puts " //    |"
-		puts "-------^-------"
-                puts "AGHHHHH YOU LOSE!"
-		end
-	end
+  if @counter == 5
+    puts <<~MESSAGE
+        ______
+        |   \|
+             |
+             |
+             |
+             |
+      -------^-------
+    MESSAGE
+  elsif @counter == 4
+    puts <<~MESSAGE
+        ______
+        |   \|
+        O    |
+             |
+             |
+             |
+      -------^-------
+    MESSAGE
+  elsif @counter == 3
+  puts <<~MESSAGE
+        ______
+        |   \|
+        O    |
+        |    |
+             |
+             |
+      -------^-------
+    MESSAGE
+  elsif @counter == 2
+  puts <<~MESSAGE
+        ______
+        |   \|
+        O    |
+       -|    |
+             |
+             |
+      -------^-------
+    MESSAGE
+  elsif @counter == 1
+  puts <<~MESSAGE
+        ______
+        |   \|
+        O    |
+       -|-   |
+             |
+             |
+      -------^-------
+    MESSAGE
+  else
+  puts <<~MESSAGE
+        ______
+        |   \|
+        O    |
+       -|-   |
+        ^    |
+       //    |
+      -------^-------
+      AGHHHHH YOU LOSE!
+    MESSAGE
+  end
+end
--- hangman.rb
+++ hangman.rb
@@ -6,269 +6,105 @@ class Hangman
   @name = name
 
   def initialize
-    puts "What is your name?"
+    puts 'What is your name?'
     @name = gets.chomp
-    puts "
-################################################
-                   HANGMAN
-################################################
-               _________
-              |        
-              |       |
-              |       O
-              |      /|\\
-              |       |
-              |      / \\
-              |
-              -----------------
-Welcome #{@name} to Hangman. The computer will generate
-a 5-12 letter random word. You will try to guess
-that word one letter at a time. Try to solve the
-puzzle before time runs out!
-\n 
-"
-  
+    puts <<~MESSAGE
+      ################################################
+                         HANGMAN
+      ################################################
+                     _________
+                    |
+                    |       |
+                    |       O
+                    |      /|\
+                    |       |
+                    |      / \
+                    |
+                    -----------------
+      Welcome #{@name} to Hangman. The computer will generate
+      a 5-12 letter random word. You will try to guess
+      that word one letter at a time. Try to solve the
+      puzzle before time runs out!
+    MESSAGE
   end
 end
 
 class Gameplay
   attr_accessor :array, :filestuff, :random_word, :cipher, :random_word2, :counter
-  def initialize
-  @array = []
-  @filestuff = File.foreach('5text.txt') do |x|
-      chomped = x.chomp
-      @array << chomped if (chomped.length >= 5 and chomped.length <= 12)
-    end
-  @random_word = @array.sample
-  @cipher = @random_word.gsub(/[a-z]/, '*').split(//)
-  @random_word2 = @random_word.split(//)
-  @counter = 5
-  puts "Would you like to load a saved game? Y/N"
-  saved_game = gets.chomp
-  if saved_game == "Y"
-    game_file = File.read("saved.json")
-    data = JSON.parse(game_file)
-    @filestuff = data["filestuff"]
-    @random_word = data["random_word"]
-    @cipher = data["cipher"]
-    @random_word2 = data["random_word2"]
-    @counter = data["counter"]
-  end
-  puts @cipher.join
-  puts "Enter a letter."
-  
-  def to_json
-    JSON.generate({filestuff: @filestuff, random_word: @random_word, cipher: @cipher, random_word2: @random_word2})
-  end
 
-  def save_game(string) 
-    File.open("saved.json", "w") do |game_file|
-  game_file.write(string)
+  def initialize
+    @array = []
+
+    @filestuff = File.foreach('5text.txt') do |x|
+      chomped = x.chomp
+      @array << chomped if chomped.length.between?(5, 12)
     end
-  end
- 
-  def choice(n)
-    @random_word2.each_with_index do |i,index|
-      if i == n
-        @cipher[index] = i 
-      end 
-    end 
-      if n == @random_word2.join.to_s
-        puts "You win"
-        puts "would you like to start another game? Y/N"
+
+    @random_word = @array.sample
+    @cipher = @random_word.gsub(/[a-z]/, '*').split(//)
+    @random_word2 = @random_word.split(//)
+    @counter = 5
+
+    puts 'Would you like to load a saved game? Y/N'
+    saved_game = gets.chomp
+    if saved_game == 'Y'
+      game_file = File.read('saved.json')
+      data = JSON.parse(game_file)
+      @filestuff = data['filestuff']
+      @random_word = data['random_word']
+      @cipher = data['cipher']
+      @random_word2 = data['random_word2']
+      @counter = data['counter']
+    end
+    puts @cipher.join
+    puts 'Enter a letter.'
+
+    def to_json
+      JSON.generate(filestuff: @filestuff, random_word: @random_word, cipher: @cipher, random_word2: @random_word2)
+    end
+
+    def save_game(string)
+      File.open('saved.json', 'w') { |game_file| game_file.write(string) }
+    end
+
+    def choice(n)
+      @random_word2.each_with_index { |i, index| @cipher[index] = i if i == n }
+
+      if n == @random_word2.join
+        puts 'You win'
+        puts 'would you like to start another game? Y/N'
         new_game = gets.chomp
-        if new_game == "Y"
+        if new_game == 'Y'
           Hangman.new
-          else exit 
+        else
+          exit
         end
       end
+
       if @random_word2.include?(n) == false
         @counter -= 1
         display
         puts "#{@counter} guesses remaining."
       end
+
       if @counter == 0
-        puts "would you like to start another game? Y/N"
+        puts 'would you like to start another game? Y/N'
         new_game = gets.chomp
-        if new_game == "Y"
-          else exit 
-        end
-      end 
-      if n == "1"
-          save_game(to_json)
-        end 
-        puts @cipher.join
-        puts "Want to save? Press 1 to save."    
-  end
+        exit if new_game != 'Y'
+      end
 
-  @counter = 5
-  while @counter > 0 
-    choice(gets.chomp)
-  end
+      save_game(to_json) if n == '1'
 
- end 
+      puts @cipher.join
+      puts 'Want to save? Press 1 to save.'
+    end
+
+    @counter = 5
+    while @counter > 0
+      choice(gets.chomp)
+    end
+  end
 end
+
 Hangman.new
 Gameplay.new
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
